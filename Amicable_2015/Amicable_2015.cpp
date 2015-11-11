51cb0564afc106a2e4c9d964a1c0b860c00e4f84
SChernykh <sergey.v.chernykh@gmail.com>
2015-11-11 22:16:44 +0100

    Create Amicable_2015.cpp

---
+++ Amicable_2015/Amicable_2015.cpp
@@ -0,0 +1,49 @@
+#include "stdafx.h"
+#include "PrimeTables.h"
+#include "Tests.h"
+#include "Engine.h"
+
+int main(int argc, char* argv[])
+{
+	PrimeTablesInit(false);
+
+	if ((argc >= 2) && (strcmp(argv[1], "/test") == 0))
+	{
+		if (!TestCheckPair())
+		{
+			std::cerr << "CheckPair test failed" << std::endl;
+			return 1;
+		}
+		if (!TestMaximumSumOfDivisors3())
+		{
+			std::cerr << "MaximumSumOfDivisors3 test failed" << std::endl;
+			return 2;
+		}
+		std::cout << "All tests passed" << std::endl;
+		return 0;
+	}
+
+	number numThreads = 0;
+	if ((argc == 3) && (strcmp(argv[1], "/threads") == 0))
+	{
+		numThreads = static_cast<number>(atoi(argv[2]));
+		if (numThreads > 64)
+			numThreads = 64;
+	}
+
+#if RUN_PERFORMANCE_TEST
+	for (;;)
+#endif
+	{
+		LARGE_INTEGER f, t1, t2;
+		QueryPerformanceFrequency(&f);
+		QueryPerformanceCounter(&t1);
+		const number n = RunSearch(numThreads);
+		QueryPerformanceCounter(&t2);
+		printf("completed in %f seconds\n%I64u pairs found\n%e CPU cycles\n\n",
+				static_cast<double>(t2.QuadPart - t1.QuadPart) / f.QuadPart,
+				n,
+				static_cast<double>(TotalCPUcycles)
+		);
+	}
+}
