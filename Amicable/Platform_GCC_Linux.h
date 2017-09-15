ab6fb8c783a611823998c68d27bb97565a92506c
SChernykh <sergey.v.chernykh@gmail.com>
2017-09-15 07:22:53 +0200

    Fixed clock_gettime bug in MacOS

--- Amicable/Platform_GCC_Linux.h
+++ Amicable/Platform_GCC_Linux.h
@@ -94,18 +94,38 @@ FORCEINLINE int LeaveCriticalSection(CRITICAL_SECTION* lock)
 #define TRY
 #define EXCEPT(X)
 
+#ifdef __MACH__
+#include <mach/clock.h>
+#include <mach/mach.h>
+#endif
+
+FORCEINLINE void current_utc_time(timespec *ts)
+{
+#ifdef __MACH__ // OS X does not have clock_gettime, use clock_get_time
+	clock_serv_t cclock;
+	mach_timespec_t mts;
+	host_get_clock_service(mach_host_self(), CALENDAR_CLOCK, &cclock);
+	clock_get_time(cclock, &mts);
+	mach_port_deallocate(mach_task_self(), cclock);
+	ts->tv_sec = mts.tv_sec;
+	ts->tv_nsec = mts.tv_nsec;
+#else
+	clock_gettime(CLOCK_REALTIME, ts);
+#endif
+}
+
 class Timer
 {
 public:
 	FORCEINLINE explicit Timer()
 	{
-		clock_gettime(CLOCK_REALTIME, &t1);
+		current_utc_time(&t1);
 	}
 
 	FORCEINLINE double getElapsedTime() const
 	{
 		timespec t2;
-		clock_gettime(CLOCK_REALTIME, &t2);
+		current_utc_time(&t2);
 		return (t2.tv_sec - t1.tv_sec) + (t2.tv_nsec - t1.tv_nsec) * 1e-9;
 	}
 
